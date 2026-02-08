9fd09833cdd40f4772f4e38e071c6c7f97e8b874
Amano07 <amanyo3010@gmail.com>
2026-02-08 16:51:00 +0100

    Profiling using nvtx

--- first_kernel/02_kernels/03_mat_mul.cu
+++ first_kernel/02_kernels/03_mat_mul.cu
@@ -14,7 +14,7 @@ void matmul_cpu(float *A, float *B, float *C, int m, int k, int n){
         for(int j=0; j < n; j++){
             float sum = 0.0f;
             for(int l=0; l < k; l++){
-                sum += A[i * k + l] + B[l * n + j];
+                sum += A[i * k + l] * B[l * n + j];
             }
             C[i * n + j] = sum;
         }
@@ -28,7 +28,7 @@ __global__ void matmul_gpu(float *A, float *B, float *C, int m, int k, int n){
     if(row < m && col < n){
         float sum = 0.0f;
         for(int l=0; l < k; l++){
-            sum += A[row * k + l] + B[l * n + col];
+            sum += A[row * k + l] * B[l * n + col];
         }
         C[row * n + col] = sum;
     }
